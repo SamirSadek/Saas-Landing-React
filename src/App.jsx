9f32d8f02bf1a513a283c808eccf5a59f7e9d3f6
Samir Sadek <samirsadek2009@gmail.com>
2025-03-07 06:05:53 +0600

    Download and Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Download from "./sections/Download.jsx";
 import Faq from "./sections/Faq.jsx";
 import Features from "./sections/Features.jsx";
+import Footer from "./sections/Footer.jsx";
 import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Pricing from "./sections/Pricing.jsx";
+import Testimonials from "./sections/Testimonials.jsx";
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq/>
+      <Testimonials/>
+      <Download/>
+      <Footer/>
     </main>
   );
 };
